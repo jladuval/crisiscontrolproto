9f04476233f1c5e07dc2cf143b9f06fc60d401cc
Jacob <jladuval@gmail.com>
2013-11-15 18:49:09 -0500

    animations are working

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -1,49 +1,32 @@
-var CCMobile = Ember.Application.create({
-    LOG_TRANSITIONS: true,
-    LOG_BINDINGS: true,
-    LOG_VIEW_LOOKUPS: true,
-    LOG_STACKTRACE_ON_DEPRECATION: true,
-    LOG_VERSION: true,
-    debugMode: true
+App = Ember.Application.create();
+
+
+App.Router.reopen({
+    location: 'history'
 });
-
-CCMobile.Router.map(function () { 
-    this.resource("help");
-    this.resource("canyouhelp");
-    this.resource("canthelp");
-    this.resource("canhelp");
+App.Router.map(function() {
+    this.route("needhelp");
+    this.route("canhelp");
 });
-
-CCMobile.HelpView = Ember.View.extend();
-
-CCMobile.Question = Ember.Object.extend({
-    number: 0,
-    maxTime: 0,
-    questionText: "",
-});
-
-var Questions = Ember.A([
-        CCMobile.Question.create({
-            number: 1,
-            questionText: "How old are you1?"
-        }),
-         CCMobile.Question.create({
-            number: 2,
-            questionText: "How old are you2?"
-        }), 
-        CCMobile.Question.create({
-            number: 3,
-            questionText: "How old are you3?"
-        })]);
-
-CCMobile.QuestionController = Ember.Controller.extend({
-    questions: Questions,
-    currentQuestionNo : 0,
-    currentQuestion: Questions[0],
-    actions: {
-        next: function() {
-            this.set("currentQuestionNo", this.currentQuestionNo + 1);
-            this.set("currentQuestion", this.questions[this.currentQuestionNo]);
+App.ApplicationRoute = Ember.Route.extend({
+    events: {
+        goToNeedHelp: function(){
+            this.transitionToAnimated('needhelp',  {main: 'slideLeft'}); 
+        },
+        goToCanHelp: function(){
+            this.transitionToAnimated('canhelp',  {main: 'slideLeft'}); 
+        },
+        goBack: function() {
+            Ember.AnimatedContainerView.enqueueAnimations({main: 'slideRight'});
+            window.history.go(-1);
         }
     }
 });
+
+
+App.ApplicationView = Ember.View.extend({
+    classNames: ['application']
+});
+
+App.CanhelpView = Ember.View.extend();
+App.NeedhelpView = Ember.View.extend();
